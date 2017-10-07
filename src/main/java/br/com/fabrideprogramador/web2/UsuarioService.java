b824ae08ef70e53ca3eec3b4cb01da7a425ce527
virmerson <virmerson@hightechcursos.com.br>
2017-10-07 15:12:30 -0400

    Criando UsuarioService

---
+++ src/main/java/br/com/fabrideprogramador/web2/UsuarioService.java
@@ -0,0 +1,61 @@
+package br.com.fabrideprogramador.web2;
+
+import br.com.fabrideprogramador.web2.model.Perfil;
+import br.com.fabrideprogramador.web2.model.Usuario;
+import br.com.fabrideprogramador.web2.repository.PerfilRepository;
+import br.com.fabrideprogramador.web2.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by Virmerson Bento dos Santos on 07/10/17.
+ */
+@Service
+public class UsuarioService  {
+
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    @Autowired
+    private PerfilRepository perfilRepository;
+
+
+
+    public Usuario salvar (Usuario usuario){
+
+        if (usuario.getPerfil()==null){
+            Perfil perfilPadrao = perfilRepository.findOne(2);
+            usuario.setPerfil(perfilPadrao);
+        }
+      return  usuarioRepository.save(usuario);
+    }
+
+    public void excluir(Integer id) {
+     usuarioRepository.delete(id);
+    }
+
+    public List<Usuario> buscarPorNome(String nome) {
+        return usuarioRepository.findByNomeIgnoreCase(nome);
+    }
+
+    public List<Usuario> buscarPorNomeESenha(String nome, String senha) {
+        return usuarioRepository.findByNomeAndSenha(nome, senha);
+    }
+
+    public Usuario buscarPorId(Integer id) {
+        return usuarioRepository.findOne(id);
+    }
+
+
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public List<Usuario> buscarPorPerfil(String descricao) {
+        return usuarioRepository.findByPerfilDescricao(descricao);
+    }
+}
--- src/main/java/br/com/fabrideprogramador/web2/controller/UsuarioController.java
+++ src/main/java/br/com/fabrideprogramador/web2/controller/UsuarioController.java
@@ -2,6 +2,9 @@ package br.com.fabrideprogramador.web2.controller;
 
 import java.util.List;
 
+import br.com.fabrideprogramador.web2.UsuarioService;
+import br.com.fabrideprogramador.web2.model.Perfil;
+import br.com.fabrideprogramador.web2.repository.PerfilRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,46 +15,51 @@ import br.com.fabrideprogramador.web2.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/usucontroller")
-@CrossOrigin(origins = {"http://127.0.0.1:52282"} )
+@CrossOrigin(origins = {"*"} )
 public class UsuarioController {
 
+
+
 	@Autowired
-	private UsuarioRepository usuarioRepository;
-	
-	
+	private UsuarioService  usuarioService;
+
+
 	@PostMapping
 	public Usuario inserir(@RequestBody Usuario usuario){
-		return usuarioRepository.save(usuario);
+
+		return usuarioService.salvar(usuario);
 	}
 	
 	@PutMapping
 	public Usuario alterar(@RequestBody Usuario usuario){
-		return usuarioRepository.save(usuario);
+
+
+		return usuarioService.salvar(usuario);
 	}
 	
 	@DeleteMapping("/{id}")
 	public void excluir(@PathVariable("id") Integer id){
-		usuarioRepository.delete(id);
+		usuarioService.excluir(id);
 	}
 	
 	@GetMapping("/{id}")
 	public Usuario buscarPorId(@PathVariable("id") Integer id){
-		return usuarioRepository.findOne(id);
+		return usuarioService.buscarPorId(id);
 	}
 	@GetMapping("/q/{nome}")
 	public List<Usuario> buscarPorNome(@PathVariable("nome")  String nome){
-		return usuarioRepository.findByNomeIgnoreCase(nome);
+		return usuarioService.buscarPorNome(nome);
 	}
 
 	@GetMapping("/q2")
 	public List<Usuario> buscarPorNomeESenha(@RequestParam("nome") String nome, @RequestParam("senha") String senha){
-		return usuarioRepository.findByNomeAndSenha(nome, senha);
+		return usuarioService.buscarPorNomeESenha(nome, senha);
 	}
 
 
 	@PostMapping ("/autenticar")
 	public List<Usuario> autenticar(@RequestBody Usuario usuario) throws UsuarioNaoEncontradoException {
-		List<Usuario> l  =  usuarioRepository.findByNomeAndSenha(usuario.getNome(),usuario.getSenha());
+		List<Usuario> l  =  usuarioService.buscarPorNomeESenha(usuario.getNome(),usuario.getSenha());
 
 		if  (l==null || l.size()==0){
 			throw new UsuarioNaoEncontradoException();
@@ -63,12 +71,12 @@ public class UsuarioController {
 
 	@GetMapping
 	public List<Usuario> buscarTodos(){
-		return usuarioRepository.findAll();
+		return usuarioService.buscarTodos();
 	}
 
 	@GetMapping ("/q/perfil")
 	List<Usuario> buscarPorPerfil(@RequestParam("descricao") String descricao ){
-		return usuarioRepository.buscarPorPerfil(descricao);
+		return usuarioService.buscarPorPerfil(descricao);
 	}
 
 }
